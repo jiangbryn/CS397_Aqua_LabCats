5270e96d38d53af5c16a22f196f3c815669fe779
Su Kang <sukang2017@u.northwestern.edu>
2019-10-25 18:27:36 -0500

    close me button centered

--- material-dashboard-react/src/components/StudyTabs/Popup.js
+++ material-dashboard-react/src/components/StudyTabs/Popup.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Mycalendar from 'components/Calendar/calendars';
+import Mycalendar from "components/Calendar/calendars";
 import Button from "components/CustomButtons/Button.js";
+import Typography from "@material-ui/core/Typography";
 
-const Popup = ({closePopup}) => {
-    return (
-      <div>
-        <Mycalendar />
-        <Button color="info" onClick={closePopup}>Close Me</Button>
-      </div>
-    );
-}
+const Popup = ({ closePopup }) => {
+  return (
+    <div>
+      <Mycalendar />
+      <Typography paragraph style={{ textAlign: "center" }}>
+        <Button color="info" onClick={closePopup}>
+          Close Me
+        </Button>
+      </Typography>
+    </div>
+  );
+};
 
 export default Popup;
